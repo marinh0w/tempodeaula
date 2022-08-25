f79fb67b291f197c69e3b452ae9f6fff2613b5d4
marinh0w <gabrielgambardella14@gmail.com>
2022-08-25 17:25:53 -0300

    add views

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('Index')
+  getIndex():{
+  // getHello(): string {
+  //   return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [RenderModule],
   controllers: [AppController],
   providers: [AppService],
 })
