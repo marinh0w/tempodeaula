3ca9c39a8830dc8c97376bd87606e089be7ea717
marinh0w <gabrielgambardella14@gmail.com>
2022-08-26 17:28:20 -0300

    identacao

--- component/CustomTable.js
+++ component/CustomTable.js
@@ -1,70 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import DataTable from 'react-data-table-component'
+import DataTable from 'react-data-table-component';
 
 const CustomTable = () => {
-    const [countries,setCountries] =useState([]); 
-     
-    const getCountries = async () => {
-        try {
-            const response = await axios.get("https://restcountries.com/v2/all")
-            setCountries(response.data);
-            console.log(countries)
-        } catch (error) {
-                console.log(error);
-        }
-    }  
-      const columns = [
-        {
-          name: "Horário",
-          selector: row => row.name
-          
-        },
-        {
-          name: "Segunda",
-          selector: row => row.name 
-        },
-        {
-          name: "Terça",
-          selector: row => row.capital,
-          conditionalCellStyles:[ /* usar para os horários disponíveis */
-          {
-              when: row => row.capital === "Kabul",
-              style: {
-                backgroundColor: "blue"
-              }
-          }
-          ]
-          
-        },
-        {
-          name: "Quarta",
-          selector: row => row.name 
-        },
-        {
-          name: "Quinta",
-          selector: row => row.name
-        },
-        {
-          name: "Sexta",
-          selector: row => row.name
-        },
-        {
-          name: "Flag",
-          selector: row => <img src={row.flag} width={50} height={50} />
-        },
+  const [countries, setCountries] = useState([]);
 
-      ]
+  const getCountries = async () => {
+    try {
+      const response = await axios.get('https://restcountries.com/v2/all');
+      setCountries(response.data);
+      console.log(countries);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const columns = [
+    {
+      name: 'Horário',
+      selector: (row) => row.name,
+    },
+    {
+      name: 'Segunda',
+      selector: (row) => row.name,
+    },
+    {
+      name: 'Terça',
+      selector: (row) => row.capital,
+      conditionalCellStyles: [
+        /* usar para os horários disponíveis */
+        {
+          when: (row) => row.capital === 'Kabul',
+          style: {
+            backgroundColor: 'blue',
+          },
+        },
+      ],
+    },
+    {
+      name: 'Quarta',
+      selector: (row) => row.name,
+    },
+    {
+      name: 'Quinta',
+      selector: (row) => row.name,
+    },
+    {
+      name: 'Sexta',
+      selector: (row) => row.name,
+    },
+    {
+      name: 'Flag',
+      selector: (row) => <img src={row.flag} width={50} height={50} />,
+    },
+  ];
 
-
-    useEffect(()=>{
-        getCountries();
-    },[])        
+  useEffect(() => {
+    getCountries();
+  }, []);
   return (
     <div>
-      <DataTable columns={columns} data={countries} pagination selectableRows fixedHeader />
+      <DataTable
+        columns={columns}
+        data={countries}
+        pagination
+        selectableRows
+        fixedHeader
+      />
     </div>
-  )
-}
+  );
+};
 
-export default CustomTable
+export default CustomTable;
---
+++ pages/views/index.tsx
@@ -0,0 +1,14 @@
+import * as React from'react';
+import Head from 'next/head';
+
+export const Index = () => <div>
+    <Head>
+        <title>Hello World</title>
+    </Head>
+    <h1>HW</h1>
+    <div>
+         
+    </div>
+</div>
+
+export default Index;
